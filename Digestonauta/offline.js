302bd9ea59976f2da9a76d74b9dea7f94b419cb4
Aegor-Stark30 <50372296+Aegor-Stark30@users.noreply.github.com>
2021-03-15 22:13:31 -0300

    Add files via upload

---
+++ Digestonauta/offline.js
@@ -0,0 +1,11 @@
+﻿{
+	"version": 1615854987,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fumaça.png",
+		"images/explosão.png"
+	]
+}
